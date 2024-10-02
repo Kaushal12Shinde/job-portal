353850c363cc49bbc4a4002e78b7e23535b5b764
kaushal12shinde <kaushushinde12@gmail.com>
2024-10-02 22:45:35 +0530

    fix / multer middleware

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { login, logout, register, updateProfile } from '../controller/user.controller.js';
 import { isAuthenticated } from '../middlewares/isAuthenticated.js';
+import { singleUpload } from '../middlewares/multer.js';
 
 const router = express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post(singleUpload,register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
-router.route("/update/profle").put(isAuthenticated,updateProfile);
+router.route("/update/profle").put(isAuthenticated, singleUpload ,updateProfile);
 
 export default router; 
