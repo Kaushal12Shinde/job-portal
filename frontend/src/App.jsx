7c48645d0116eb75fb509370a1f22a520d07d05b
Kaushal12Shinde <kaushushinde12@gmail.com>
2024-10-07 19:12:25 +0530

    fix/ routing for new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
+import Jobs from './components/Jobs'
 
 const appRouter = createBrowserRouter([
   {
@@ -16,7 +17,22 @@ const appRouter = createBrowserRouter([
   {
     path: '/signup',
     element: <Signup/>,
-  }
+  },
+  {
+    path:'/jobs',
+    element: <Jobs/>
+  },
+  {
+    path: '/description/:id',
+  },
+  {
+    path: "/browse",
+    element: <Browse />
+  },
+  {
+    path: "/profile",
+    element: <Profile />
+  },
 ])
 
 function App() {
